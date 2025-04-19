6d8a643e9fb411ba1ac77d90427c4abaee3effc5
Li Heng <symxll@126.com>
2025-04-19 21:36:06 +0800

    手搓导航栏和路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
+import { useState } from 'react'
+import { Reshaped, View, Avatar, Button } from 'reshaped'
+import { X, Square, Minus, Settings, Sun, Moon } from 'react-feather'
 import { HashRouter } from 'react-router-dom'
-import { Reshaped } from 'reshaped'
 import AppRoutes from './routes'
-import 'reshaped/themes/figma/theme.css';
 
-const App = () => (
-  <Reshaped theme='figma'>
-    <HashRouter>
-      <AppRoutes />
-    </HashRouter>
-  </Reshaped>
-)
+import 'reshaped/themes/figma/theme.css'
+import 'reshaped/themes/reshaped/theme.css'
+import 'reshaped/themes/fragments/twitter/theme.css'
+import 'reshaped/themes/slate/theme.css'
+
+const App = () => {
+  const [isDark, setIsDark] = useState(false)
+
+  return (
+    <Reshaped>
+      <nav>
+        <View h-full w-full direction='row' justify='space-between'>
+          <View direction='row' padding={1}>
+            <Avatar size={7} />
+            {/* 页面栈位置 */}
+          </View>
+          {/* 用户和设置 */}
+          <View direction='row' gap={3}>
+            <View direction='row' gap={1}>
+              <Button color='primary' variant='ghost' icon={<Sun />} />
+              <Button color='primary' variant='ghost' icon={<Settings />} />
+              <Button color='primary' variant='ghost' icon={<Avatar size={7} src='https://avatars.githubusercontent.com/u/133302251?v=4' />} />
+            </View>
+            {/* 右上角三按钮 */}
+            <View direction='row' gap={1}>
+              <Button color='primary' variant='ghost' icon={<Minus />} />
+              <Button color='primary' variant='ghost' icon={<Square />} />
+              <Button color='primary' variant='ghost' icon={<X />} />
+            </View>
+          </View>
+        </View>
+      </nav>
+      <main w-full h-full>
+        <HashRouter>
+          <AppRoutes />
+        </HashRouter>
+      </main>
+    </Reshaped>
+  )
+}
 
 export default App
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Home from '../pages'
+import Index from '../pages'
 
 const AppRoutes = ()=> (
   <Routes>
-    <Route path='/' element={<Home />} />
+    <Route path='/' element={<Index />}>
+    </Route>
   </Routes>
 )
 
